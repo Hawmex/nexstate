b5afb223f9903a2bccf976080451018c2eb09ed4
Hawmex <hawmed.a@gmail.com>
2021-08-07 11:55:12 +0430

    moved forward

--- src/nexstate.ts
+++ src/nexstate.ts
@@ -1,26 +1,24 @@
 import { Nexbounce } from 'nexbounce';
 
+export type NexstateOptions = { logger?: boolean };
+export type SyncAction<T> = (state: T) => T;
+export type AsyncAction<T> = (state: T) => Promise<T>;
 export type Subscription<T> = (state: T) => void;
 export type SubscribeOptions = { signal?: AbortSignal };
 
 export class Nexstate<T> {
   #state: T;
+  #options?: NexstateOptions;
   #publishDebouncer = new Nexbounce();
   #subscriptions = new Set<Subscription<T>>();
 
-  constructor(defaultState: T) {
+  constructor(defaultState: T, options?: NexstateOptions) {
     this.#state = defaultState;
+    this.#options = options;
   }
 
   get state(): T {
     return this.#state;
-  }
-
-  set state(value: T) {
-    if (value !== this.state) {
-      this.#state = value;
-      this.#publish();
-    }
   }
 
   #publish() {
@@ -31,6 +29,39 @@ export class Nexstate<T> {
 
   #unsubscribe(subscription: Subscription<T>) {
     this.#subscriptions.delete(subscription);
+  }
+
+  #log(prevState: T) {
+    console.groupCollapsed('Nexstate Logger');
+    console.log('%c Previous State:', '', prevState);
+    console.log('%c Current State:', '', this.state);
+    console.groupEnd();
+  }
+
+  setState(action: SyncAction<T>): void;
+  setState(action: AsyncAction<T>): Promise<void>;
+  setState(action: any): any {
+    const prevState = this.state;
+
+    const output = action(prevState);
+
+    if (output instanceof Promise)
+      return new Promise<void>(async (resolve) => {
+        this.#state = await output;
+
+        if (this.#options?.logger) this.#log(prevState);
+
+        this.#publish();
+
+        resolve();
+      });
+    else {
+      this.#state = output;
+
+      if (this.#options?.logger) this.#log(prevState);
+
+      this.#publish();
+    }
   }
 
   subscribe(subscription: Subscription<T>, options?: SubscribeOptions) {
@@ -45,6 +76,7 @@ export class Nexstate<T> {
 
   runAndSubscribe(subscription: Subscription<T>, options?: SubscribeOptions) {
     if (!options?.signal?.aborted) subscription(this.state);
+
     this.subscribe(subscription, options);
   }
 }
